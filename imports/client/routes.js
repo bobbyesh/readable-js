ba14b281640770b2f85ab6cb51485e413479f288
Bobby Eshleman <eshleman@pdx.edu>
2017-10-16 08:39:39 -0700

    Hooked up React routing

---
+++ imports/client/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Router, Route } from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory';
+
+// route components
+import App from '../ui/App';
+
+const browserHistory = createBrowserHistory();
+export const renderRoutes = () => (
+  <Router history={browserHistory}>
+    <div>
+      <Route exact path="/" component={App}/>
+    </div>
+  </Router>
+);
